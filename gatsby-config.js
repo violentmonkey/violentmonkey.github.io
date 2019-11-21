62029ccba221a7e3981d38c563312f3045df0938
Gerald <gera2ld@163.com>
2019-11-21 11:11:34 +0800

    fix: add attributes to external links

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,6 +65,7 @@ module.exports = {
           },
           'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
+          'gatsby-remark-external-links',
         ],
       },
     },
