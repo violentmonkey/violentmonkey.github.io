aa845d13b37856ad8eedb789680625032822f29a
Gerald <gera2ld@live.com>
2021-06-03 23:48:34 +0800

    chore: upgrade deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,38 +79,7 @@ module.exports = {
         exclude: ['/auth_**'],
       },
     },
-    {
-      resolve: 'gatsby-plugin-sitemap',
-      options: {
-        query: `
-            {
-              site {
-                siteMetadata {
-                  siteUrl
-                }
-              }
-              allSitePage(
-                filter: {
-                  path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
-                }
-              ) {
-                edges {
-                  node {
-                    path
-                  }
-                }
-              }
-          }`,
-        output: '/sitemap.xml',
-        serialize: ({ site, allSitePage }) => allSitePage.edges.map(edge => {
-          return {
-            url: site.siteMetadata.siteUrl + edge.node.path,
-            changefreq: 'daily',
-            priority: 0.7,
-          };
-        }),
-      },
-    },
+    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
