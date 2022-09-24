2acdc0111e02936771adb912a8c74636e36b7929
Gerald <gera2ld@live.com>
2022-09-24 23:20:07 +0800

    chore: remove invalid pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,13 @@
+const path = require('path');
+
 exports.onCreateNode = async ({ node, getNode, actions }) => {
   if (node.internal.type === 'Mdx') {
     const parent = getNode(node.parent);
-    const slug = [
-      parent.relativeDirectory,
-      parent.name === 'index' ? '' : parent.name,
-    ].filter(Boolean).join('/') + '/';
+    let slug =
+      [parent.relativeDirectory, parent.name === 'index' ? '' : parent.name]
+        .filter(Boolean)
+        .join('/');
+    if (slug && !slug.endsWith('/')) slug += '/';
     actions.createNodeField({
       node,
       name: 'slug',
@@ -14,36 +17,64 @@ exports.onCreateNode = async ({ node, getNode, actions }) => {
 };
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createRedirect } = actions;
-  const {
-    data: {
-      allMdx: {
-        nodes,
+  {
+    const {
+      data: {
+        allMdx: { nodes },
       },
-    },
-  } = await graphql(`
-    query {
-      allMdx {
-        nodes {
-          fields {
-            slug
-          }
-          frontmatter {
-            redirect_from
+    } = await graphql(`
+      query {
+        allMdx {
+          nodes {
+            fields {
+              slug
+            }
+            frontmatter {
+              redirect_from
+            }
           }
         }
       }
-    }
-  `);
-  nodes.forEach(node => {
-    const toPath = `/${node.fields.slug}`;
-    node.frontmatter.redirect_from?.forEach(fromPath => {
-      createRedirect({
-        fromPath,
-        toPath,
-        isPermanent: true,
-        redirectInBrowser: true
+    `);
+    nodes.forEach((node) => {
+      const toPath = `/${node.fields.slug}`;
+      node.frontmatter.redirect_from?.forEach((fromPath) => {
+        actions.createRedirect({
+          fromPath,
+          toPath,
+          isPermanent: true,
+          redirectInBrowser: true,
+        });
       });
     });
-  });
+  }
+  {
+    const { data, errors } = await graphql(`
+      query {
+        allMdx(filter: { fields: { slug: { ne: null } } }) {
+          nodes {
+            id
+            fields {
+              slug
+            }
+            internal {
+              contentFilePath
+            }
+          }
+        }
+      }
+    `);
+    if (errors) throw errors;
+    const templatePost = path.resolve('src/templates/post/index.js');
+    data.allMdx.nodes.forEach((node) => {
+      const slug = node.fields.slug;
+      actions.createPage({
+        path: `/${slug}`,
+        component: `${templatePost}?__contentFilePath=${node.internal.contentFilePath}`,
+        context: {
+          id: node.id,
+        },
+      });
+    });
+  }
 };
